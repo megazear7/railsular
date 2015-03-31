59c74edb1c2bb10f3f6ecf9c6809c71abfbc5419
alex-lockhart <alockhart@totalsim.us>
2015-03-31 15:49:00 -0400

    creating plots based off of added simulations and the chosen y var

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,23 +1,34 @@
 class Result < ActiveRecord::Base
   has_and_belongs_to_many :simulations
 
-  def csv_file_path
-    File.join(ENV['HOME'], "/crimson_files/", 'test.csv')
+  def csv_file_path sim
+    File.join(ENV['HOME'], "/crimson_files/", App.find(1).name.downcase.tr(' ', '_'), sim.job_directory_name, 'results', 'data', 'iterative.csv')
   end
 
   def csv_data
-    if File.exists?(csv_file_path)
-      source = CSV.open(csv_file_path)
-      CSV.generate do |csv|
-        source.each do |row|
-          csv << row
-        end
+    cols = []
+    row_count = 0
+    simulations.each do |sim|
+      col_data = []
+      CSV.foreach(csv_file_path(sim), {headers: true}) do |row|
+        row_count += 1
+        col_data << row[y_var]
       end
-    else
-      # TODO we need a better way to deal with the file not existing
-      CSV.generate do |csv|
-        csv << ["No File"]
-        csv << ["0"]
+      cols << col_data
+    end
+
+    CSV.generate do |csv|
+      header_row = ["x"]
+      simulations.each do |sim|
+        header_row << sim.name
+      end
+      csv << header_row
+      (1..row_count).each do |row|
+        row_data = [row]
+        cols.each do |col|
+          row_data << col[row]
+        end
+        csv << row_data
       end
     end
   end
